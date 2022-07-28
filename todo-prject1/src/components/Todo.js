4b43893fbf8c796f91a0c5a58f74f925573f553c
wahid-ahmed <wahidahmed1992@gmail.com>
2022-07-28 11:33:00 +0600

    install fontawesome

--- todo-prject1/src/components/Todo.js
+++ todo-prject1/src/components/Todo.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const Todo = (props) => {
     const {id,title,desc}=props.todo;
   return (
     <article>
-      <div>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-      </div>
+        <div>
+            <h3>{title}</h3>
+            <p>{desc}</p>
+        </div>
+        <div>
+            <i className='fa fa-trash fa-2x'></i>
+        </div>
     </article>
   )
 }
