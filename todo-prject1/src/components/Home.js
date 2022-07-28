b1e474cb1a7b091001469ca1506f084e21cd1570
wahid-ahmed <wahidahmed1992@gmail.com>
2022-07-28 10:46:13 +0600

    Create home components

---
+++ todo-prject1/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const dumyTodos=[
+    {
+        id:1,
+        title:'title 1',
+        desc:'descripton 1 for todo project'
+    },
+    {
+        id:2,
+        title:'title 2',
+        desc:'descripton 2 for todo project'
+    }
+]
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
